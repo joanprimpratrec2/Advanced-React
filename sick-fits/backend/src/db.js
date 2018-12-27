68cdaf587bb2ed56d56fb48b149540de19078ae7
joanprim <joanprimpratrec2@gmail.com>
2018-12-27 07:58:21 +0100

    GraphQL Yoga server running
    after some updated and env problems solved. Now server is running... I hope

---
+++ sick-fits/backend/src/db.js
@@ -0,0 +1,13 @@
+// This file connects to the remote prisma DB and gives us the ability to query it with JS
+const { Prisma } = require('prisma-binding');
+
+const db = new Prisma({
+  typeDefs: 'src/generated/prisma.graphql',
+  endpoint: process.env.PRISMA_ENDPOINT,
+  secret: process.env.PRISMA_SECRET,
+  debug: false,
+});
+
+module.exports = db;
+
+"https://us1.prisma.sh/joanprimpratrec2-7b0c75/sicccck-fits/dev"

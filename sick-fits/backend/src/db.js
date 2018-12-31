410bd3cda00a15b671f0e656cbee7a418b7faaa3
joanprim <joanprimpratrec2@gmail.com>
2018-12-31 08:59:35 +0100

    end video 14- query an mutations working
    all backend work is working!!!

--- sick-fits/backend/src/db.js
+++ sick-fits/backend/src/db.js
@@ -4,10 +4,8 @@ const { Prisma } = require('prisma-binding');
 const db = new Prisma({
   typeDefs: 'src/generated/prisma.graphql',
   endpoint: process.env.PRISMA_ENDPOINT,
-  secret: process.env.PRISMA_SECRET,
+ 
   debug: false,
 });
 
 module.exports = db;
-
-"https://us1.prisma.sh/joanprimpratrec2-7b0c75/sicccck-fits/dev"
